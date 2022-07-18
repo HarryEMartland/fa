f9b76ec80ee7a0fc179d2cb9934bcbda7693a700
Harry Martland <HarryEMartland@gmail.com>
2022-07-18 21:53:39 +0100

    changed data to use classes

--- src/Definitions.tsx
+++ src/Definitions.tsx
@@ -1,40 +1,21 @@
 
 import { AcronymList } from './AcronymList'
-import { drabc, sample, socrates } from './Data'
+import { allAcronyms } from './Data'
 import React from 'react'
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
 import { ExpandMore } from '@mui/icons-material'
 export function Definitions () {
   return <>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMore/>}
-                aria-controls="panel1a-content">
-                <Typography>DRsABCs</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <AcronymList data={drabc}/>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMore/>}
-                aria-controls="panel2a-content">
-                <Typography>SAMPLE</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <AcronymList data={sample}/>
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMore/>}
-                aria-controls="panel2a-content">
-                <Typography>SOCRATES</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <AcronymList data={socrates}/>
-            </AccordionDetails>
-        </Accordion>
+      {allAcronyms.map(acronym => <Accordion key={acronym.name}>
+          <AccordionSummary
+              expandIcon={<ExpandMore/>}
+              aria-controls="panel1a-content">
+              <Typography>{acronym.name}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+              <AcronymList data={acronym.letters}/>
+          </AccordionDetails>
+      </Accordion>)}
+
     </>
 }
